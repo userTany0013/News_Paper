d882b26e1515f0f19aa0b233ef918e7497364437
userTany0013 <tany1ghom@gmail.com>
2025-03-26 22:13:00 +0300

    22.Итоговое задание (HW-03).2

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -162,7 +162,7 @@ ACCOUNT_FORMS = {'signup': 'news.forms.CommonSignupForm'}
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = 'Tany911922@yandex.ru'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = 'wnaemjzhgmnczrhh'
 EMAIL_USE_SSL = True
 
 APSCHEDULER_DATETIME_FORMAT = "N j, Y, f:s a"
